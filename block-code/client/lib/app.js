c1a457245fe2f39fbc80aaf53abb02ad94c1457f
Ivan Miranda <ivan.i.n@hotmail.com>
2016-04-02 20:55:51 -0300

    panzoom widget

--- block-code/client/lib/app.js
+++ block-code/client/lib/app.js
@@ -1,4 +1,4 @@
 (function () {
     'use strict';
-    angular.module('block-code', ['angular-meteor', 'accounts.ui', 'angular-meteor.auth', 'ui.router', 'dndLists', 'panzoom']);
+    angular.module('block-code', ['angular-meteor', 'accounts.ui', 'angular-meteor.auth', 'ui.router', 'dndLists', 'panzoom', 'panzoomwidget']);
 }());
