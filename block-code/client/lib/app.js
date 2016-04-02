7b71cad65f23aa2dc29a3f8d37c6ac0a1d977ebc
Ivan Miranda <ivan.i.n@hotmail.com>
2016-04-02 20:17:46 -0300

    personagem show

--- block-code/client/lib/app.js
+++ block-code/client/lib/app.js
@@ -1,4 +1,4 @@
 (function () {
     'use strict';
-    angular.module('block-code', ['angular-meteor', 'accounts.ui', 'angular-meteor.auth', 'ui.router', 'dndLists']);
+    angular.module('block-code', ['angular-meteor', 'accounts.ui', 'angular-meteor.auth', 'ui.router', 'dndLists', 'panzoom']);
 }());
--- block-code/client/programa-controller.js
+++ block-code/client/programa-controller.js
@@ -4,6 +4,53 @@
     angular.module('block-code').controller('ProgramaController', ['$scope', function ($scope) {
         $scope.selected = null;
 
+        $scope.tabuleiro = {
+            tamanho: {
+                x: 10,
+                y: 10
+            }
+        };
+
+        $scope.personagem = {
+            direcao: 'cima',
+            posicao: {
+                x: 0,
+                y: 0
+            }
+        };
+
+        $scope.direcaoToIcon = function direcaoToIcon() {
+            /*jslint white:true*/
+            switch ($scope.personagem.direcao) {
+                case 'cima':
+                    return 'fa fa-arrow-circle-o-up';
+                case 'baixo':
+                    return 'fa fa-arrow-circle-o-down';
+                case 'direita':
+                    return 'fa fa-arrow-circle-o-right';
+                case 'esquerda':
+                    return 'fa fa-arrow-circle-o-left';
+            }
+        };
+
+        $scope.quadros = [];
+
+        var x, y, quadro;
+        for (x = 0; x < $scope.tabuleiro.tamanho.x; x += 1) {
+            $scope.quadros.push([]);
+            for (y = 0; y < $scope.tabuleiro.tamanho.y; y += 1) {
+                quadro = {
+                    personagem: false
+                };
+
+                if ($scope.personagem.posicao.x === x && $scope.personagem.posicao.y === y) {
+                    quadro.personagem = true;
+                }
+
+                $scope.quadros[x].push(quadro);
+            }
+        }
+
         $scope.pecas = [
             {
                 label: 'Andar',
@@ -45,5 +92,8 @@
         $scope.executar = function () {
 
         };
+
+        $scope.panzoomConfig = {};
+        $scope.panzoomModel = {};
     }]);
 }());
