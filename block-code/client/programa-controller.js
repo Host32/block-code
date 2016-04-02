77e6a8fcb8c504747562538eed48dbea915b3d1c
Ivan Miranda <ivan.i.n@hotmail.com>
2016-04-02 18:56:29 -0300

    pecas coloridas

--- block-code/client/programa-controller.js
+++ block-code/client/programa-controller.js
@@ -7,18 +7,35 @@
         $scope.pecas = [
             {
                 label: 'Andar',
+                icon: 'fa fa-arrow-up',
+                background: '#0792A9',
+                badgeBackground: '#138D74',
                 comando: 'andar'
             },
             {
                 label: 'Girar para direita',
+                icon: 'fa fa-rotate-right',
+                background: '#D73858',
+                badgeBackground: '#7C2C2F',
                 comando: 'girarDireita'
             },
             {
                 label: 'Girar para esquerda',
+                icon: 'fa fa-rotate-left',
+                background: '#FBD8A0',
+                badgeBackground: '#D4892E',
                 comando: 'girarEsquerda'
             }
         ];
 
         $scope.programa = [];
+
+        $scope.reiniciar = function () {
+            $scope.programa = [];
+        };
+
+        $scope.executar = function () {
+
+        };
     }]);
 }());
