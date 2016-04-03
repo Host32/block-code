34cee9b1abd7255b55ca7ab9f4c301eba4050821
Donatello Ferraz <donatelloferraz@hotmail.com>
2016-04-03 13:03:45 -0300

    Cadastro tabuleiro controler e cadastro de tabuleiros.

---
+++ block-code/client/cadastro-tabuleiro-controller.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    angular.module('block-code').controller('CadastroTabuleiroController', ['$scope', '$log', function ($scope, $log) {
+
+        $scope.tabuleiro = {
+            name: '',
+            descricao: '',
+            quadros: [{
+                x: 0,
+                y: 0,
+                inacessivel: false,
+                pontodechegada: false
+            }]
+        };
+
+        $scope.addQuadro = function () {
+            $scope.tabuleiro.quadros.push({});
+        };
+
+        $scope.removeQuadro = function ($index) {
+            $scope.tabuleiro.quadros.splice($index, 1);
+        };
+
+        $scope.insereNovoQuadro = function () {
+
+            Tabuleiros.insert({
+                name: $scope.tabuleiro.name,
+                descricao: $scope.tabuleiro.descricao
+            }, function (err, tabuleiroInserido) {
+                Quadros.insert($scope.tabuleiro.quadros.map(function (quadro) {
+                    return {
+                        tabuleiroId: tabuleiroInserido._id,
+                        x: quadro.x,
+                        y: quadro.y,
+                        inacessivel: quadro.inacessivel,
+                        pontodechegada: quadro.pontodechegada
+                    };
+                }));
+
+            });
+        };
+
+    }]);
+}());
